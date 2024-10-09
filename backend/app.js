7856f96b4bd1838c54ce062d1b00afa52ee3ef5c
Mr. S <555sfg555@gmail.com>
2024-10-09 20:45:09 +0200

    Error (404, 500) handling added.

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,19 @@ app.get('/', (req, res) => {
     res.json(tempResponse.responseDef)
 })
 
+// Middleware for handling 404 errors
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Not Found" });
+    throw new Error("Something went wrong!"); 
+  });
+// General error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error for debugging
+    res.status(500).json({ error: "Internal Server Error" });
+    throw new Error("Something went wrong!"); 
+  });
+  
+
 app.listen(port, () => {
     console.log(`Server is crashing at  127.0.0.1:${ port }`)
 })
